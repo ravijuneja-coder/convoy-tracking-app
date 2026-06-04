620898fb50e4397b827a255242d803ef3a5c8f18
Ravi Juneja <ravijuneja1986@gmail.com>
2026-06-04 19:44:08 +0530

    Auto-save: 2026-06-04 19:44:08

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export function getRecaptchaVerifier(containerId) {
+  return new RecaptchaVerifier(auth, containerId, { size: "invisible" });
+}
